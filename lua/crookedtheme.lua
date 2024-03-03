09969fd43458962870a9af997f0c50a264250bca
Ethan-Heimer <ethan.heimer12357@gmail.com>
2024-03-02 17:40:19 -0700

    Config Setup

--- lua/crookedtheme.lua
+++ lua/crookedtheme.lua
@@ -2,9 +2,14 @@
 local config = require("crookedtheme.config")
 print(config.test)
 
+vim.api.nvim_set_hl(0, "@function", {fg = config.functionColor})
 vim.api.nvim_set_hl(0, "@method", {fg = config.functionColor})
 vim.api.nvim_set_hl(0, "@string", {fg = config.stringColor})
 vim.api.nvim_set_hl(0, "@boolean", {fg = config.booleanColor})
+vim.api.nvim_set_hl(0, "@attribute.diff", {fg = config.attributeColor})
+vim.api.nvim_set_hl(0, "@class", {fg = config.classColor})
+vim.api.nvim_set_hl(0, "@conditional", {fg = config.conditionalColor})
+vim.api.nvim_set_hl(0, "@field", {fg = config.fieldColor})
 
 function test()
     bool = true;
--- lua/crookedtheme/config.lua
+++ lua/crookedtheme/config.lua
@@ -4,5 +4,8 @@ config.attributeColor = "#FFFF00"
 config.booleanColor = "#FF0000"
 config.functionColor = "#FFFFAA"
 config.stringColor = "#FFAAFF"
+config.classColor = "#FFEE00"
+config.conditionalColor = "#9900AA"
+config.fieldColor = "#BBDD90" 
 
 return config
