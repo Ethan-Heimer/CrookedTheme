9156340ee3742526b2506d75e49e73d90d3d6f4d
Ethan-Heimer <ethan.heimer12357@gmail.com>
2024-03-03 18:18:15 -0700

    Setup Config

--- lua/crookedtheme/config.lua
+++ lua/crookedtheme/config.lua
@@ -1,30 +1,30 @@
 config = {}
 
-config.attributeColor = {fg = "#FFFF00"}
-config.booleanColor = {fg = "#FF0000"}
-config.functionColor = {fg = "#FFFFAA"}
-config.stringColor = {fg = "#FFAAFF"}
-config.classColor = {fg = "#FFEE00"}
-config.conditionalColor = {fg = "#9900AA"}
-config.fieldColor = {fg = "#BBDD90"} 
-config.moduleColor = {fg = "#FF99CC"}
-config.macroColor = {fg = "#CCDDAA"}
-config.typeColor = {fg = "#FF0033"}
-config.textColor = {fg = "#FF3399"}
-config.keywordColor = {fg = "#F343D3"}
-config.builtinFunctionColor = {fg = "#F30099"}
-config.operatorColor = {fg = "#334455"}
-config.variableColor = {fg = "#00FFD3"}
-config.specialTextColor = {fg = "#447788"}
-config.commentColor = {fg = "#889922"}
-config.commentWarningColor = {fg = "#667709"}
-config.commentTodoColor = {fg = "#DD5577"}
-config.commentErrorColor = {fg = "#556883"}
-config.commentNoteColor = {fg = "#123456"}
-config.characterColor = {fg = "#485760"}
-config.constantColor = {fg = "#DDDD55"}
-config.netrwDirectoryColor = {fg = "#324272"}
-config.netrwPlainColor = {fg = "#216191"}
-config.symbolColor = {fg = "#45FF00"}
+config.attributeColor = {fg = "#FFFFFF"}
+config.booleanColor = {fg = "#FFFFFF"}
+config.functionColor = {fg = "#FFFFFF"}
+config.stringColor = {fg = "#FFFFFF"}
+config.classColor = {fg = "#FFEEFF"}
+config.conditionalColor = {fg = "#FFFFFF"}
+config.fieldColor = {fg = "#FFFFFF"} 
+config.moduleColor = {fg = "#FFFFFF"}
+config.macroColor = {fg = "#FFFFFF"}
+config.typeColor = {fg = "#FFFFFF"}
+config.textColor = {fg = "#FFFFFF"}
+config.keywordColor = {fg = "#FFFFFF"}
+config.builtinFunctionColor = {fg = "#FFFFFF"}
+config.operatorColor = {fg = "#FFFFFF"}
+config.variableColor = {fg = "#FFFFFF"}
+config.specialTextColor = {fg = "#FFFFFF"}
+config.commentColor = {fg = "#FFFFFF"}
+config.commentWarningColor = {fg = "#FFFFFF"}
+config.commentTodoColor = {fg = "#FFFFFF"}
+config.commentErrorColor = {fg = "#FFFFFF"}
+config.commentNoteColor = {fg = "#FFFFFF"}
+config.characterColor = {fg = "#FFFFFF"}
+config.constantColor = {fg = "#FFFFFF"}
+config.netrwDirectoryColor = {fg = "#FFFFFF"}
+config.netrwPlainColor = {fg = "#FFFFFF"}
+config.symbolColor = {fg = "#FFFFFF"}
 
 return config
