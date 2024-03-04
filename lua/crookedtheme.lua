e6cff985a5b92a193e07c3b9d6054fd1e13f342c
Ethan-Heimer <ethan.heimer12357@gmail.com>
2024-03-03 18:07:35 -0700

    Setup Config

--- lua/crookedtheme.lua
+++ lua/crookedtheme.lua
@@ -1,44 +1,43 @@
 --:Telescope highlights
 local config = require("crookedtheme.config")
-print(config.test)
 
-vim.api.nvim_set_hl(0, "Function", {fg = config.functionColor, bold = true})
+vim.api.nvim_set_hl(0, "Function", config.functionColor)
 vim.api.nvim_set_hl(0, "@function", {link = "Function"})
 vim.api.nvim_set_hl(0, "@function.method", {link = "Function"})
 vim.api.nvim_set_hl(0, "@method", {link = "Function"})
-vim.api.nvim_set_hl(0, "@function.builtin", {fg = config.builtinFunctionColor, bold = true})
+vim.api.nvim_set_hl(0, "@function.builtin", config.builtinFunctionColor)
 
-vim.api.nvim_set_hl(0, "Type", {fg = config.typeColor})
+vim.api.nvim_set_hl(0, "Type", config.typeColor)
 vim.api.nvim_set_hl(0, "@type", {link = "Type"})
 
-vim.api.nvim_set_hl(0, "@string", {fg = config.stringColor})
-vim.api.nvim_set_hl(0, "@boolean", {fg = config.booleanColor, bold = true})
-vim.api.nvim_set_hl(0, "@attribute.diff", {fg = config.attributeColor})
-vim.api.nvim_set_hl(0, "@class", {fg = config.classColor})
-vim.api.nvim_set_hl(0, "@conditional", {fg = config.conditionalColor})
-vim.api.nvim_set_hl(0, "@field", {fg = config.fieldColor})
-vim.api.nvim_set_hl(0, "@include", {fg = config.moduleColor})
-vim.api.nvim_set_hl(0, "@property", {fg = config.fieldColor})
-vim.api.nvim_set_hl(0, "Constant", {fg = config.constantColor})
+vim.api.nvim_set_hl(0, "@string", config.stringColor)
+vim.api.nvim_set_hl(0, "@boolean", config.booleanColor)
+vim.api.nvim_set_hl(0, "@attribute.diff", config.attributeColor)
+vim.api.nvim_set_hl(0, "@class", config.classColor)
+vim.api.nvim_set_hl(0, "@conditional", config.conditionalColor)
+vim.api.nvim_set_hl(0, "@field", config.fieldColor)
+vim.api.nvim_set_hl(0, "@include", config.moduleColor)
+vim.api.nvim_set_hl(0, "@property", config.fieldColor)
+vim.api.nvim_set_hl(0, "Constant", config.constantColor)
 
-vim.api.nvim_set_hl(0, "Text", {fg = config.textColor})
+vim.api.nvim_set_hl(0, "Text", config.textColor)
 vim.api.nvim_set_hl(0, "@text", {link = "Text"})
 vim.api.nvim_set_hl(0, "Identifier", {link = "Text"})
 
-vim.api.nvim_set_hl(0, "Special", {fg = config.specialTextColor})
+vim.api.nvim_set_hl(0, "Special", config.specialTextColor)
 vim.api.nvim_set_hl(0, "@text.uri", {link = "Special"})
 vim.api.nvim_set_hl(0, "@text.title", {link = "Special"})
 
-vim.api.nvim_set_hl(0, "Comment", {fg = config.commentColor})
+vim.api.nvim_set_hl(0, "Comment", config.commentColor)
 vim.api.nvim_set_hl(0, "@comment", {link = "Comment"})
-vim.api.nvim_set_hl(0, "@comment.warning", {fg = config.commentWarningColor})
-vim.api.nvim_set_hl(0, "@comment.error", {fg = config.commentErrorColor})
-vim.api.nvim_set_hl(0, "@comment.todo", {fg = config.commentTodoColor})
-vim.api.nvim_set_hl(0, "@comment.note", {fg = config.commentNoteColor})
+vim.api.nvim_set_hl(0, "@comment.warning", config.commentWarningColor)
+vim.api.nvim_set_hl(0, "@comment.error", config.commentErrorColor)
+vim.api.nvim_set_hl(0, "@comment.todo", config.commentTodoColor)
+vim.api.nvim_set_hl(0, "@comment.note", config.commentNoteColor)
 
 vim.api.nvim_set_hl(0, "@character", {link = "Constant"})
 
-vim.api.nvim_set_hl(0, "Statement", {fg = config.keywordColor, bold = true, italic = true})
+vim.api.nvim_set_hl(0, "Statement", config.keywordColor)
 vim.api.nvim_set_hl(0, "@keyword", {link = "Keyword"})
 vim.api.nvim_set_hl(0, "@keyword.return", {link = "@keyword"})
 vim.api.nvim_set_hl(0, "@keyword.import", {link = "@keyword"})
@@ -52,20 +51,16 @@ vim.api.nvim_set_hl(0, "@keyword.conditional", {link = "@keyword"})
 vim.api.nvim_set_hl(0, "@keyword.conditional.ternary", {link = "@keyword"})
 vim.api.nvim_set_hl(0, "@keyword.directive.define", {link = "@operator"})
 
-vim.api.nvim_set_hl(0, "Operator", {fg = config.operatorColor})
+vim.api.nvim_set_hl(0, "Operator", config.operatorColor)
 vim.api.nvim_set_hl(0, "@operator", {link = "Operator"})
 vim.api.nvim_set_hl(0, "@keyword.operator", {link = "@operator"})
 
-vim.api.nvim_set_hl(0, "@variable", {fg = config.variableColor})
-vim.api.nvim_set_hl(0, "PreProc", {fg = config.macroColor})
+vim.api.nvim_set_hl(0, "@variable", config.variableColor)
+vim.api.nvim_set_hl(0, "PreProc", config.macroColor)
 
-vim.api.nvim_set_hl(0, "netrwDir", {fg = config.netrwDirectoryColor})
-vim.api.nvim_set_hl(0, "netrwPlain", {fg = config.netrwPlainColor})
-vim.api.nvim_set_hl(0, "Delimiter", {fg = config.netrwPlainColor})
-:q
-:q
-:w
-
+vim.api.nvim_set_hl(0, "netrwDir", config.netrwDirectoryColor)
+vim.api.nvim_set_hl(0, "netrwPlain", config.netrwPlainColor)
+vim.api.nvim_set_hl(0, "Delimiter", config.symbolColor)
 
 
 
--- lua/crookedtheme/config.lua
+++ lua/crookedtheme/config.lua
@@ -1,29 +1,30 @@
 config = {}
 
-config.attributeColor = "#FFFF00"
-config.booleanColor = "#FF0000"
-config.functionColor = "#FFFFAA"
-config.stringColor = "#FFAAFF"
-config.classColor = "#FFEE00"
-config.conditionalColor = "#9900AA"
-config.fieldColor = "#BBDD90" 
-config.moduleColor = "#FF99CC"
-config.macroColor = "#CCDDAA"
-config.typeColor = "#FF0033"
-config.textColor = "#FF3399"
-config.keywordColor = "#F343D3"
-config.builtinFunctionColor = "#F30099"
-config.operatorColor = "#334455"
-config.variableColor = "#00FFD3"
-config.specialTextColor = "#447788"
-config.commentColor = "#889922"
-config.commentWarningColor = "#667709"
-config.commentTodoColor = "#DD5577"
-config.commentErrorColor = "#556883"
-config.commentNoteColor = "#123456"
-config.characterColor = "#485760"
-config.constantColor = "#DDDD55"
-config.netrwDirectoryColor = "#324272"
-config.netrwPlainColor = "#216191"
-config.symbolColor = "#45FF00"
+config.attributeColor = {fg = "#FFFF00"}
+config.booleanColor = {fg = "#FF0000"}
+config.functionColor = {fg = "#FFFFAA"}
+config.stringColor = {fg = "#FFAAFF"}
+config.classColor = {fg = "#FFEE00"}
+config.conditionalColor = {fg = "#9900AA"}
+config.fieldColor = {fg = "#BBDD90"} 
+config.moduleColor = {fg = "#FF99CC"}
+config.macroColor = {fg = "#CCDDAA"}
+config.typeColor = {fg = "#FF0033"}
+config.textColor = {fg = "#FF3399"}
+config.keywordColor = {fg = "#F343D3"}
+config.builtinFunctionColor = {fg = "#F30099"}
+config.operatorColor = {fg = "#334455"}
+config.variableColor = {fg = "#00FFD3"}
+config.specialTextColor = {fg = "#447788"}
+config.commentColor = {fg = "#889922"}
+config.commentWarningColor = {fg = "#667709"}
+config.commentTodoColor = {fg = "#DD5577"}
+config.commentErrorColor = {fg = "#556883"}
+config.commentNoteColor = {fg = "#123456"}
+config.characterColor = {fg = "#485760"}
+config.constantColor = {fg = "#DDDD55"}
+config.netrwDirectoryColor = {fg = "#324272"}
+config.netrwPlainColor = {fg = "#216191"}
+config.symbolColor = {fg = "#45FF00"}
+
 return config
