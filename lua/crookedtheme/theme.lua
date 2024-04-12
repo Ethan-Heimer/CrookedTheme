5659ff55510bc6774f926878ae7d654f26d1874f
Ethan-Heimer <ethan.heimer12357@gmail.com>
2024-04-12 09:56:16 -0700

    Config Rework

--- lua/crookedtheme/theme.lua
+++ lua/crookedtheme/theme.lua
@@ -1,6 +1,35 @@
 --:Telescope highlights
-local config = require"./config"
-print(config.functionColor)
+
+local config = {}
+
+config.attributeColor = {fg = "#FFFFFF"}
+config.booleanColor = {fg = "#FFFFFF"}
+config.functionColor = {fg = "#FFFFFF"}
+config.stringColor = {fg = "#FFFFFF"}
+config.classColor = {fg = "#FFEEFF"}
+config.conditionalColor = {fg = "#FFFFFF"}
+config.fieldColor = {fg = "#FFFFFF"} 
+config.moduleColor = {fg = "#FFFFFF"}
+config.macroColor = {fg = "#FFFFFF"}
+config.typeColor = {fg = "#FFFFFF"}
+config.textColor = {fg = "#FFFFFF"}
+config.keywordColor = {fg = "#FFFFFF"}
+config.builtinFunctionColor = {fg = "#FFFFFF"}
+config.operatorColor = {fg = "#FFFFFF"}
+config.variableColor = {fg = "#FFFFFF"}
+config.specialTextColor = {fg = "#FFFFFF"}
+config.commentColor = {fg = "#FFFFFF"}
+config.commentWarningColor = {fg = "#FFFFFF"}
+config.commentTodoColor = {fg = "#FFFFFF"}
+config.commentErrorColor = {fg = "#FFFFFF"}
+config.commentNoteColor = {fg = "#FFFFFF"}
+config.characterColor = {fg = "#FFFFFF"}
+config.constantColor = {fg = "#FFFFFF"}
+config.netrwDirectoryColor = {fg = "#FFFFFF"}
+config.netrwPlainColor = {fg = "#FFFFFF"}
+config.symbolColor = {fg = "#FFFFFF"}
+config.normalColor = {bg = "#000000"}
+
 function Apply()
     vim.api.nvim_set_hl(0, "Function", config.functionColor)
     vim.api.nvim_set_hl(0, "@function", {link = "Function"})
@@ -67,5 +96,6 @@ function Apply()
 end
 
 return {
-    Apply = Apply
+    Apply = Apply,
+    Config = config
 }
