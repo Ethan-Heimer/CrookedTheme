95e4cd911bd3a3b426bae33c863c4f62e21bba61
Ethan-Heimer <ethan.heimer12357@gmail.com>
2024-06-17 13:56:39 -0700

    FEAT: Exposes More Highlight Groups

--- lua/crookedtheme/init.lua
+++ lua/crookedtheme/init.lua
@@ -6,7 +6,7 @@ config.attributeColor = {fg = "#FFFFFF"}
 config.booleanColor = {fg = "#FFFFFF"}
 config.functionColor = {fg = "#FFFFFF"}
 config.stringColor = {fg = "#FFFFFF"}
-config.classColor = {fg = "#FFEEFF"}
+config.classColor = {fg = "#FFFFFF"}
 config.conditionalColor = {fg = "#FFFFFF"}
 config.fieldColor = {fg = "#FFFFFF"} 
 config.moduleColor = {fg = "#FFFFFF"}
@@ -29,6 +29,10 @@ config.netrwDirectoryColor = {fg = "#FFFFFF"}
 config.netrwPlainColor = {fg = "#FFFFFF"}
 config.symbolColor = {fg = "#FFFFFF"}
 config.normalColor = {bg = "#000000"}
+config.columnLineColor = {bg = "#000000"}
+config.lineNumber = {fg = "#FFFFFF"}
+config.nonText = {fg = "#FFFFFF"}
+config.menuColor = {bg = "#FFFFFF", fg = "#000000"}
 
 function Apply()
     vim.api.nvim_set_hl(0, "Function", config.functionColor)
@@ -36,14 +40,21 @@ function Apply()
     vim.api.nvim_set_hl(0, "@function.method", {link = "Function"})
     vim.api.nvim_set_hl(0, "@method", {link = "Function"})
     vim.api.nvim_set_hl(0, "@function.builtin", config.builtinFunctionColor)
+    vim.api.nvim_set_hl(0, "CmpItemKindFunctionDefault", {link = "Function"})
+    vim.api.nvim_set_hl(0, "CmpItemKindMethodDefault", {link = "Function"})
 
     vim.api.nvim_set_hl(0, "Type", config.typeColor)
     vim.api.nvim_set_hl(0, "@type", {link = "Type"})
+    vim.api.nvim_set_hl(0, "CmpItemKindTypeParameterDefault", {link = "Type"})
 
     vim.api.nvim_set_hl(0, "@string", config.stringColor)
+    vim.api.nvim_set_hl(0, "CmpItemKindStringDefault", {link = "@string"})
+
     vim.api.nvim_set_hl(0, "@boolean", config.booleanColor)
     vim.api.nvim_set_hl(0, "@attribute.diff", config.attributeColor)
     vim.api.nvim_set_hl(0, "@class", config.classColor)
+    vim.api.nvim_set_hl(0, "CmpItemKindClassDefault", {link = "@class"})
+
     vim.api.nvim_set_hl(0, "@conditional", config.conditionalColor)
     vim.api.nvim_set_hl(0, "@field", config.fieldColor)
     vim.api.nvim_set_hl(0, "@include", config.moduleColor)
@@ -53,6 +64,7 @@ function Apply()
     vim.api.nvim_set_hl(0, "Text", config.textColor)
     vim.api.nvim_set_hl(0, "@text", {link = "Text"})
     vim.api.nvim_set_hl(0, "Identifier", {link = "Text"})
+    vim.api.nvim_set_hl(0, "CmpItemKindTextDefault", {link = "Text"})
 
     vim.api.nvim_set_hl(0, "Special", config.specialTextColor)
     vim.api.nvim_set_hl(0, "@text.uri", {link = "Special"})
@@ -87,14 +99,20 @@ function Apply()
 
     vim.api.nvim_set_hl(0, "@variable", config.variableColor)
     vim.api.nvim_set_hl(0, "PreProc", config.macroColor)
+    vim.api.nvim_set_hl(0, "CmpItemKindVariableDefault", {link = "@variable"})
 
     vim.api.nvim_set_hl(0, "netrwDir", config.netrwDirectoryColor)
     vim.api.nvim_set_hl(0, "netrwPlain", config.netrwPlainColor)
     vim.api.nvim_set_hl(0, "Delimiter", config.symbolColor)
 
     vim.api.nvim_set_hl(0, "Normal", config.normalColor)
-end
 
+    vim.api.nvim_set_hl(0, "ColorColumn", config.columnLineColor)
+    vim.api.nvim_set_hl(0, "LineNr", config.lineNumber)
+    vim.api.nvim_set_hl(0, "NonText", config.nonText)
+    vim.api.nvim_set_hl(0, "Pmenu", config.menuColor)
+end
+Apply()
 return {
     Apply = Apply,
     Config = config
