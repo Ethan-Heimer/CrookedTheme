e96f336803b7d90a66f88839c40f6478b2249379
Ethan-Heimer <ethan.heimer12357@gmail.com>
2024-08-12 22:38:42 -0700

    REF: Reorganizing Highlight Groups

--- lua/crookedtheme/functions.lua
+++ lua/crookedtheme/functions.lua
@@ -3,9 +3,11 @@ local Themes = {}
 
 function NewTheme()
     local config = {}
+
     local base = {}
     local editor = {}
     local netrw = {}
+    local keyword = {}
 
     config.name = "New Theme"
     config.description = "New Crooked Theme"
@@ -20,6 +22,7 @@ function NewTheme()
     base.constant = {fg = "#FFFFFF"}
     base.preproc = {fg = "#FFFFFF"}
     base.symbol = {fg = "#FFFFFF"}
+    base.boolean = {fg = "#FFFFFF"}
 
     editor.lineNumber = {fg = "#FFFFFF"}
     editor.colorColumn = {fg = "#FFFFFF"}
@@ -46,9 +49,13 @@ function NewTheme()
     netrw.change = {fg = "#FFFFFF"}
     netrw.cursorLine = {fg = "#FFFFFF"}
 
+    keyword.method = nil;
+    keyword.modifier = nil;
+
     config.base = base;
     config.editor = editor;
     config.netrw = netrw;
+    config.keyword = keyword;
 
     return config
 end
@@ -87,13 +94,13 @@ function Apply(theme)
     vim.api.nvim_set_hl(0, "Constant", theme.base.constant)
     vim.api.nvim_set_hl(0, "PreProc", theme.base.preproc)
     vim.api.nvim_set_hl(0, "Special", theme.base.symbol)
+    vim.api.nvim_set_hl(0, "Boolean", theme.base.boolean)
 
     --Editor Themes
     vim.api.nvim_set_hl(0, "LineNr", theme.editor.lineNumber)
     vim.api.nvim_set_hl(0, "ColorColumn", theme.editor.colorColumn)
     vim.api.nvim_set_hl(0, "Normal", theme.editor.background)
     vim.api.nvim_set_hl(0, "CursorColumn", theme.editor.cursorColumn)
-    --vim.api.nvim_set_hl(0, "CursorLine", {link = "CursorColumn"}) netrw
     vim.api.nvim_set_hl(0, "FoldColumn", {link = "CursorColumn"})
     vim.api.nvim_set_hl(0, "SignColumn", {link = "CursorColumn"})
     vim.api.nvim_set_hl(0, "Conceal", {link = "Cursorcolumn"})
@@ -121,97 +128,14 @@ function Apply(theme)
     vim.api.nvim_set_hl(0, "netrwTimeSep", theme.netrw.time)
     vim.api.nvim_set_hl(0, "DiffChange", theme.netrw.change)
 
-    --[[
-    vim.api.nvim_set_hl(0, "Function", theme.functionColor)
-    vim.api.nvim_set_hl(0, "@function", {link = "Function"})
-    vim.api.nvim_set_hl(0, "@function.method", {link = "Function"})
-    vim.api.nvim_set_hl(0, "@method", {link = "Function"})
-    vim.api.nvim_set_hl(0, "@function.builtin", theme.builtinFunctionColor)
-    vim.api.nvim_set_hl(0, "CmpItemKindFunctionDefault", {link = "Function"})
-    vim.api.nvim_set_hl(0, "CmpItemKindMethodDefault", {link = "Function"})
+    --Keywords
+    if(theme.keyword.method ~= nil) then
+        vim.api.nvim_set_hl(0, "@keyword.function", theme.keyword.method)
+    end
 
-    vim.api.nvim_set_hl(0, "Type", theme.typeColor)
-    vim.api.nvim_set_hl(0, "@type", {link = "Type"})
-    vim.api.nvim_set_hl(0, "CmpItemKindTypeParameterDefault", {link = "Type"})
-
-    vim.api.nvim_set_hl(0, "@string", theme.stringColor)
-    vim.api.nvim_set_hl(0, "CmpItemKindStringDefault", {link = "@string"})
-
-    vim.api.nvim_set_hl(0, "@boolean", theme.booleanColor)
-    vim.api.nvim_set_hl(0, "@attribute.diff", theme.attributeColor)
-
-    vim.api.nvim_set_hl(0, "@class", theme.classColor)
-    vim.api.nvim_set_hl(0, "CmpItemKindClassDefault", {link = "@class"})
-
-    vim.api.nvim_set_hl(0, "@conditional", theme.conditionalColor)
-    vim.api.nvim_set_hl(0, "@field", theme.fieldColor)
-    vim.api.nvim_set_hl(0, "@property", theme.fieldColor)
-    vim.api.nvim_set_hl(0, "Constant", theme.constantColor)
-
-    vim.api.nvim_set_hl(0, "Text", theme.textColor)
-    vim.api.nvim_set_hl(0, "@text", {link = "Text"})
-    vim.api.nvim_set_hl(0, "Identifier", {link = "Text"})
-    vim.api.nvim_set_hl(0, "CmpItemKindTextDefault", {link = "Text"})
-
-    vim.api.nvim_set_hl(0, "Special", theme.specialTextColor)
-    vim.api.nvim_set_hl(0, "@text.uri", {link = "Special"})
-    vim.api.nvim_set_hl(0, "@text.title", {link = "Special"})
-
-    vim.api.nvim_set_hl(0, "Comment", theme.commentColor)
-    vim.api.nvim_set_hl(0, "@comment", {link = "Comment"})
-    vim.api.nvim_set_hl(0, "@comment.warning", theme.commentWarningColor)
-    vim.api.nvim_set_hl(0, "@comment.error", theme.commentErrorColor)
-    vim.api.nvim_set_hl(0, "@comment.todo", theme.commentTodoColor)
-    vim.api.nvim_set_hl(0, "@comment.note", theme.commentNoteColor)
-
-    vim.api.nvim_set_hl(0, "@character", {link = "Constant"})
-
-    vim.api.nvim_set_hl(0, "Statement", theme.keywordColor)
-    vim.api.nvim_set_hl(0, "@keyword", {link = "Keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.return", {link = "@keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.repeat", {link = "@keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.return", {link = "@keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.storage", {link = "@keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.debug", {link = "@keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.exception", {link = "@keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.directive", {link = "@keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.conditional", {link = "@keyword"})
-    vim.api.nvim_set_hl(0, "@keyword.conditional.ternary", {link = "@keyword"})
-
-    vim.api.nvim_set_hl(0, "Operator", theme.operatorColor)
-    vim.api.nvim_set_hl(0, "@operator", {link = "Operator"})
-    vim.api.nvim_set_hl(0, "@keyword.operator", {link = "@operator"})
-
-    vim.api.nvim_set_hl(0, "PreProc", theme.macroColor)
-
-    vim.api.nvim_set_hl(0, "@include", theme.moduleColor)
-    vim.api.nvim_set_hl(0, "@keyword.import", {link = "@include"})
-
-    vim.api.nvim_set_hl(0, "@variable", theme.variableColor) 
-    vim.api.nvim_set_hl(0, "CmpItemKindVariableDefault", {link = "@variable"})
-
-    vim.api.nvim_set_hl(0, "netrwDir", theme.netrwDirectoryColor)
-    vim.api.nvim_set_hl(0, "netrwPlain", theme.netrwPlainColor)
-    vim.api.nvim_set_hl(0, "Delimiter", theme.symbolColor)
-
-    vim.api.nvim_set_hl(0, "Normal", theme.normalColor)
-
-    vim.api.nvim_set_hl(0, "ColorColumn", theme.columnLineColor)
-    vim.api.nvim_set_hl(0, "LineNr", theme.lineNumber)
-    vim.api.nvim_set_hl(0, "NonText", theme.nonText)
-    vim.api.nvim_set_hl(0, "Pmenu", theme.menuColor)
-    vim.api.nvim_set_hl(0, "CmpItemAbbrDefault", theme.menuTextColor)
-
-    vim.api.nvim_set_hl(0, "CursorColumn", theme.cursorColumn)
-    vim.api.nvim_set_hl(0, "CursorLine", {link = "CursorColumn"})
-    vim.api.nvim_set_hl(0, "FoldColumn", {link = "CursorColumn"})
-    vim.api.nvim_set_hl(0, "SignColumn", {link = "CursorColumn"})
-    vim.api.nvim_set_hl(0, "Conceal", {link = "Cursorcolumn"})
-
-    vim.api.nvim_set_hl(0, "Title", theme.titleColor)
-
-    vim.api.nvim_set_hl(0, "Search", theme.searchColor)
-    ]]
+    if(theme.keyword.modifier ~= nil) then
+        vim.api.nvim_set_hl(0, "@keyword.modifier", theme.keyword.modifier)
+    end
 end
 
 return {
